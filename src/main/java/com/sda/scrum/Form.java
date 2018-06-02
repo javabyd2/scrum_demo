b2b12b03cd98af562856927c202fe855ce4a1af6
marcin-slominski <slominski.marcin@gmail.com>
2018-06-02 13:24:22 +0200

    Fix

--- src/main/java/com/sda/scrum/Form.java
+++ src/main/java/com/sda/scrum/Form.java
@@ -1,4 +1,11 @@
 package com.sda.scrum;
 
+import java.util.Scanner;
+
 public class Form {
+    public static String getSiteName() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Proszę podać adres strony: ");
+        return scanner.nextLine();
+    }
 }
--- src/main/java/com/sda/scrum/Main.java
+++ src/main/java/com/sda/scrum/Main.java
@@ -2,6 +2,5 @@ package com.sda.scrum;
 
 public class Main {
     public static void main(String[] args) {
-        
     }
 }
